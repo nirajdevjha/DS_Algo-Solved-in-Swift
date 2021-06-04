58b9f61fa6da8995f6d7201d14ff3b1021b21d68
Niraj Kumar Jha <nirajdev.jha@gmail.com>
2021-06-04 18:18:31 +0530

    Update Contents.swift

--- Linked List/NthElemFromEnd.playground/Contents.swift
+++ Linked List/NthElemFromEnd.playground/Contents.swift
@@ -4,7 +4,6 @@
  T - O(N) | S - O(1)
  **/
 
-
 class Program {
 
   class LinkedList {
@@ -18,27 +17,78 @@ class Program {
   }
 
   func removeKthNodeFromEnd(head: LinkedList, k: Int) {
-    
+
+    var first: LinkedList? = head
+    var second: LinkedList? = head
+
+    var counter: Int = 1
+
+    while counter <= k {
+        second = second?.next
+        counter += 1
+    }
+
+    //special case, when second becomes nil because k is >= no of nodes in LL.
+    if second == nil {
+        head.value = head.next?.value
+        head.next = head.next?.next
+        return
+    }
+
+    while second?.next != nil {
+        first = first?.next
+        second = second?.next
+    }
+
+    first?.next = first?.next?.next
+
   }
-    
 }
 
-var abc = "airoplanes"
-let closure = { [abc] in
-   print("I love \(abc)")
+
+
+ // Definition for singly-linked list.
+  public class ListNode {
+     public var val: Int
+    public var next: ListNode?
+      public init() { self.val = 0; self.next = nil; }
+      public init(_ val: Int) { self.val = val; self.next = nil; }
+      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+  }
+
+
+// Leetcode
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+    var first: ListNode? = head
+    var second: ListNode? = head
+
+    var counter: Int = 1
+
+    if second?.next == nil {
+        return nil
+    }
+
+    while counter <= n {
+        second = second?.next
+        counter += 1
+    }
+
+    //special case, when second becomes nil because k is >= no of nodes in LL.
+    if second == nil {
+        head?.val = head?.next?.val ?? 0
+        head?.next = head?.next?.next
+        return head
+    }
+
+    while second?.next != nil {
+        first = first?.next
+        second = second?.next
+    }
+
+    first?.next = first?.next?.next
+
+        return head
+
+    }
 }
-
-abc = "ships"
-
-closure()
-
-var thing = "airoplanes"
-
-let closure1 = {
-  print("I love \(thing)")
-}
-
-thing = "airplanes"
-
-closure() // Prints: "I love airplanes"
-
